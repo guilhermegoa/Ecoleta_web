4428d3907ff594694b2176f067f3381de5659189
Guilherme Oliveira <guilhergoa@hotmail.com>
2020-06-28 16:50:31 -0300

    fifth day

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -5,6 +5,8 @@ import { Map, TileLayer, Marker } from "react-leaflet";
 import axios from "axios";
 import { LeafletMouseEvent } from "leaflet";
 
+import Dropzone from "../../components/DropZone/index";
+
 import api from "../../services/api";
 
 import logo from "../../assets/logo.svg";
@@ -44,6 +46,7 @@ const CreatePoint = () => {
   const [cities, setCities] = useState<string[]>([]);
   const [selectedCity, setSelectedCity] = useState("0");
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
   const [selectedPosition, setSelectedPosistion] = useState<[number, number]>([
     0,
     0,
@@ -74,7 +77,7 @@ const CreatePoint = () => {
         const ufInitials = response.data.map((uf) => uf.sigla);
         setUfs(ufInitials);
       });
-  });
+  }, []);
 
   useEffect(() => {
     if (selectedUf === "0") {
@@ -136,18 +139,20 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
 
-    console.log(data);
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
 
     await api.post("points", data);
 
@@ -171,6 +176,9 @@ const CreatePoint = () => {
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
+
         <fieldset>
           <legend>
             <h2>Dados</h2>
